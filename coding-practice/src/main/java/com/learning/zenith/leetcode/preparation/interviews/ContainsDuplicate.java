94cb41c3c2e3336596879245219a04e1d65ea58e
Sohrab Alam <sohrab.alam86@gmail.com>
2026-04-12 15:57:52 +0530

    Notes & Programs

---
+++ coding-practice/src/main/java/com/learning/zenith/leetcode/preparation/interviews/ContainsDuplicate.java
@@ -0,0 +1,29 @@
+package com.learning.zenith.leetcode.preparation.interviews;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+	public static boolean containsDuplicate(int[] nums) {
+		if (nums == null || nums.length <= 1) {
+			return false;
+		}
+		Set<Integer> numSet = new HashSet<>();
+		for (int n : nums) {
+			boolean added = numSet.add(n);
+			if (!added) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4, 5, 6 };
+		boolean duplicate = containsDuplicate(nums);
+		System.out.println("duplicate exists?\nANS: " + duplicate);
+
+	}
+}
---
+++ coding-practice/src/main/java/com/learning/zenith/leetcode/preparation/interviews/ValidAnagram.java
@@ -0,0 +1,101 @@
+package com.learning.zenith.leetcode.preparation.interviews;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ValidAnagram {
+
+	/** This is my logic by brute force but not optimized. */
+	public static boolean isAnagramUsingList(String s, String t) {
+		if (s == null || t == null) {
+			return false;
+		}
+		char[] chArray1 = s.toCharArray();
+
+		List<Character> chList1 = new ArrayList<>();
+
+		for (char c : chArray1) {
+			chList1.add(c);
+		}
+
+		char[] chArray2 = t.toCharArray();
+
+		List<Character> chList2 = new ArrayList<>();
+
+		for (char c : chArray2) {
+			chList2.add(c);
+		}
+		Collections.sort(chList1);
+		Collections.sort(chList2);
+		return chList1.equals(chList2);
+	}
+
+	/**
+	 * This is ChatGpt's optimized logic applicable for strings having only
+	 * alphabets in small letters.
+	 */
+	public static boolean isAnagramUsingArray(String s, String t) {
+		if (s == null || t == null || s.length() == 0 || t.length() == 0 || s.length() != t.length()) {
+			return false;
+		}
+
+		int[] countArray = new int[26];
+
+		for (int i = 0; i < s.length(); i++) {
+			countArray[s.charAt(i) - 'a']++;
+			countArray[t.charAt(i) - 'a']--;
+		}
+
+		for (int c : countArray) {
+			if (c != 0) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * This is ChatGpt's optimized universal logic applicable for all types of
+	 * strings (case insensitive).
+	 */
+	public static boolean isAnagramUsingMap(String s, String t) {
+		if (s == null || t == null || s.length() != t.length()) {
+			return false;
+		}
+		s = s.toLowerCase();
+		t = t.toLowerCase();
+
+		Map<Character, Integer> characterFrequencyMap = new HashMap<>();
+
+		for (int i = 0; i < s.length(); i++) {
+			char ch = s.charAt(i);
+			characterFrequencyMap.put(ch, characterFrequencyMap.getOrDefault(ch, 0) + 1);
+		}
+
+		for (int i = 0; i < t.length(); i++) {
+			char ch = t.charAt(i);
+			if (!characterFrequencyMap.containsKey(ch)) {
+				return false;
+			}
+			characterFrequencyMap.put(ch, characterFrequencyMap.get(ch) - 1);
+
+			if (characterFrequencyMap.get(ch) == 0) {
+				characterFrequencyMap.remove(ch);
+			}
+
+		}
+
+		return characterFrequencyMap.isEmpty();
+	}
+
+	public static void main(String[] args) {
+		String s = "aacc", t = "ccac";
+		boolean anagram = isAnagramUsingMap(s, t);
+		System.out.println(anagram);
+	}
+
+}
