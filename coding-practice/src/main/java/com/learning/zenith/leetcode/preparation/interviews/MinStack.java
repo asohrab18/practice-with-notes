55b8011d314b820b9a8387e58a4332e9f570b94f
Sohrab Alam <sohrab.alam86@gmail.com>
2026-04-24 20:17:43 +0530

    Program and NOTES

---
+++ coding-practice/src/main/java/com/learning/zenith/leetcode/preparation/interviews/MinStack.java
@@ -0,0 +1,69 @@
+package com.learning.zenith.leetcode.preparation.interviews;
+
+import java.util.Stack;
+
+/**
+ * Leetcode Problem: 155: Solution by ChatGpt. It has time complexity =
+ * O(1)
+ */
+public class MinStack {
+
+	Stack<Integer> inputStack;
+	Stack<Integer> minValueStack;
+
+	public MinStack() {
+		inputStack = new Stack<>();
+		minValueStack = new Stack<>();
+	}
+
+	public void push(int val) {
+		inputStack.push(val);
+
+		if (minValueStack.isEmpty() || minValueStack.peek() >= val) {
+			minValueStack.push(val);
+		}
+
+	}
+
+	public void pop() {
+		if (!inputStack.isEmpty()) {
+			if (!minValueStack.isEmpty() && minValueStack.peek().equals(inputStack.peek())) {
+				minValueStack.pop();
+			}
+			inputStack.pop();
+		}
+	}
+
+	public int top() {
+		if (!inputStack.isEmpty()) {
+			return inputStack.peek();
+		}
+		return 0;
+	}
+
+	public int getMin() {
+		if (!minValueStack.isEmpty()) {
+			return minValueStack.peek();
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		MinStack minStack = new MinStack();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+
+		int min1 = minStack.getMin(); // return -3
+		System.out.println("min1 = " + min1);
+
+		minStack.pop();
+
+		int top = minStack.top(); // return 0
+		System.out.println("top = " + top);
+
+		int min2 = minStack.getMin(); // return -2
+		System.out.println("min2 = " + min2);
+
+	}
+}
---
+++ coding-practice/src/main/java/com/learning/zenith/leetcode/preparation/interviews/MinStackByBruteForce.java
@@ -0,0 +1,60 @@
+package com.learning.zenith.leetcode.preparation.interviews;
+
+import java.util.Comparator;
+import java.util.Stack;
+
+/**
+ * Leetcode Problem: 155: I created this by brute force and it is working fine
+ * but getMin() has time complexity = O(n) 
+ * because it checks all the stack elements one by one for minimum value.
+ */
+public class MinStackByBruteForce {
+
+	Stack<Integer> stack = null;
+
+	public MinStackByBruteForce() {
+		stack = new Stack<>();
+	}
+
+	public void push(int val) {
+		stack.push(val);
+	}
+
+	public void pop() {
+		if (!stack.isEmpty()) {
+			stack.pop();
+		}
+	}
+
+	public int top() {
+		if (!stack.isEmpty()) {
+			return stack.peek();
+		}
+		return 0;
+	}
+
+	public int getMin() {
+		if (!stack.isEmpty()) {
+			return stack.stream().min(Comparator.naturalOrder()).get();
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		MinStackByBruteForce minStack = new MinStackByBruteForce();
+		minStack.push(-2);
+		minStack.push(0);
+		minStack.push(-3);
+
+		int min1 = minStack.getMin(); // return -3
+		System.out.println("min1 = " + min1);
+
+		minStack.pop();
+
+		int top = minStack.top(); // return 0
+		System.out.println("top = " + top);
+
+		int min2 = minStack.getMin(); // return -2
+		System.out.println("min2 = " + min2);
+	}
+}
